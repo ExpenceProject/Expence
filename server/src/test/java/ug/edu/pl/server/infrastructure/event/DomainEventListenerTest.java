804caba34037cc780bac8fa60b394f76dbdbc0e0
Piotr Damrych <piotr.damrych2002@gmail.com>
2024-11-19 16:17:48 +0100

    fix: domain events test

--- server/src/test/java/ug/edu/pl/server/infrastructure/event/DomainEventListenerTest.java
+++ server/src/test/java/ug/edu/pl/server/infrastructure/event/DomainEventListenerTest.java
@@ -3,9 +3,9 @@ package ug.edu.pl.server.infrastructure.event;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.mock.mockito.SpyBean;
-import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.stereotype.Component;
 import ug.edu.pl.server.base.IntegrationTest;
+import ug.edu.pl.server.domain.common.event.DomainEventPublisher;
 import ug.edu.pl.server.domain.common.event.TestDomainEvent;
 
 import java.time.Duration;
@@ -20,7 +20,7 @@ import static org.testcontainers.shaded.org.awaitility.Awaitility.await;
 class DomainEventListenerTest extends IntegrationTest {
 
     @Autowired
-    ApplicationEventPublisher publisher;
+    DomainEventPublisher publisher;
 
     @SpyBean
     TestEventListener testEventListener;
@@ -28,7 +28,7 @@ class DomainEventListenerTest extends IntegrationTest {
     @Test
     void shouldListenForDomainEvents() {
         // given & when
-        publisher.publishEvent(new TestDomainEvent());
+        publisher.publish(new TestDomainEvent());
 
         // then
         await()
