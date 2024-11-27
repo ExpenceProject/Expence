48908db2cb9d9479284a6d9eeffb524511dcb621
aleksandra.mordzon <aleksandra.mordzon@defra.onmicrosoft.com>
2024-11-27 22:06:06 +0100

    added tests

---
+++ server/src/test/java/ug/edu/pl/server/domain/group/GroupFacadeTest.java
@@ -0,0 +1,88 @@
+package ug.edu.pl.server.domain.group;
+
+import org.junit.jupiter.api.Test;
+import ug.edu.pl.server.domain.group.exception.GroupNotFoundException;
+import ug.edu.pl.server.domain.user.SampleUsers;
+import ug.edu.pl.server.domain.user.TestUserConfiguration;
+import ug.edu.pl.server.domain.user.UserFacade;
+import ug.edu.pl.server.domain.user.dto.UserDto;
+
+import java.util.Set;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+class GroupFacadeTest {
+  UserFacade userFacade = new TestUserConfiguration().userFacadeForGroup();
+  GroupFacade groupFacade = new TestGroupConfiguration().groupFacade(userFacade);
+  UserDto currentUser = userFacade.create(SampleUsers.VALID_USER);
+
+  @Test
+  void shouldReturnGroupById() {
+    // given
+    var group = groupFacade.create(SampleGroups.VALID_GROUP_NO_FILE_AND_INVITEES, currentUser);
+
+    // when
+    var groupDto = groupFacade.getById(group.id());
+
+    // then
+    assertThat(groupDto).isEqualTo(group);
+  }
+
+  @Test
+  void shouldThrowExceptionWhenAskedForGroupWithIdThatDoesNotExist() {
+    // when & then
+    assertThatThrownBy(() -> groupFacade.getById(SampleGroups.ID_THAT_DOES_NOT_EXIST))
+        .isInstanceOf(GroupNotFoundException.class);
+  }
+
+  @Test
+  void shouldCreateGroupWithNoFileAndInvitees() {
+    // given
+    var groupToCreate = SampleGroups.VALID_GROUP_NO_FILE_AND_INVITEES;
+
+    // when
+    var groupDto = groupFacade.create(groupToCreate, currentUser);
+
+    // then
+    assertThat(groupDto.id()).isNotNull();
+    assertThat(groupDto.name()).isEqualTo(groupToCreate.name());
+    assertThat(groupDto.settledDown()).isFalse();
+    assertThat(groupDto.image()).isNotNull();
+    assertThat(groupDto.image().key()).isNull();
+  }
+
+  @Test
+  void shouldCreateGroupWithFileAndNoInvitees() {
+    // given
+    var groupToCreate = SampleGroups.VALID_GROUP_WITH_FILE_AND_NO_INVITEES;
+
+    // when
+    var groupDto = groupFacade.create(groupToCreate, currentUser);
+
+    // then
+    assertThat(groupDto.id()).isNotNull();
+    assertThat(groupDto.name()).isEqualTo(groupToCreate.name());
+    assertThat(groupDto.settledDown()).isFalse();
+    assertThat(groupDto.image()).isNotNull();
+    assertThat(groupDto.image().key()).isNotNull();
+  }
+
+  @Test
+  void shouldCreateGroupWithFileAndInvitees() {
+    // given
+    UserDto invitee = userFacade.create(SampleUsers.ANOTHER_VALID_USER);
+
+    var groupToCreate = SampleGroups.validGroupWithFileAndInvitees(Set.of(invitee.id()));
+
+    // when
+    var groupDto = groupFacade.create(groupToCreate, currentUser);
+
+    // then
+    assertThat(groupDto.id()).isNotNull();
+    assertThat(groupDto.name()).isEqualTo(groupToCreate.name());
+    assertThat(groupDto.settledDown()).isFalse();
+    assertThat(groupDto.image()).isNotNull();
+    assertThat(groupDto.image().key()).isNotNull();
+  }
+}
--- server/src/test/java/ug/edu/pl/server/domain/user/TestUserConfiguration.java
+++ server/src/test/java/ug/edu/pl/server/domain/user/TestUserConfiguration.java
@@ -1,11 +1,18 @@
 package ug.edu.pl.server.domain.user;
 
+import org.springframework.cache.Cache;
 import org.springframework.cache.CacheManager;
+import org.springframework.cache.caffeine.CaffeineCacheManager;
 import ug.edu.pl.server.domain.common.storage.InMemoryStorageFacade;
 
-class TestUserConfiguration {
+public class TestUserConfiguration {
 
     UserFacade userFacade(CacheManager cacheManager) {
+        return new UserConfiguration().userFacade(new InMemoryUserRepository(), new InMemoryRoleRepository(), new InMemoryStorageFacade(), cacheManager);
+    }
+
+    public UserFacade userFacadeForGroup() {
+        CacheManager cacheManager = new CaffeineCacheManager();
         return new UserConfiguration().userFacade(new InMemoryUserRepository(), new InMemoryRoleRepository(), new InMemoryStorageFacade(), cacheManager);
     }
 }
